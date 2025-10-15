f198072b617eabb9857dc5873b44858552e00fce
Digitalmastermind2 <199987346+Digitalmastermind2@users.noreply.github.com>
2025-10-14 18:50:26 -0700

    new exercies

---
+++ Exercises/ex04.js
@@ -0,0 +1,37 @@
+
+let totalNumber = 0;
+
+
+
+
+// add a button titled click me
+
+$("#needy-button").click( function(){
+
+totalNumber = totalNumber + 3;
+
+
+let sentence = "Click! ";
+let totalNumberbox= sentence + totalNumber;
+
+
+$("#needy-button").html(totalNumberbox);
+
+});
+
+
+// when it is clicked
+
+// add one to the total number
+
+//  show the total number
+
+// on our button
+
+// show "clicked total number times"
+
+
+
+
+
+// a top limit
